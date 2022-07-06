b9cbbce7466276b8b6b3232a08639fcd0d71f2d7
Plamen <biznetmen@gmail.com>
2022-07-06 12:09:04 +0300

    tokens contrac refactoring

--- migrations/3_tokens_migration.js
+++ migrations/3_tokens_migration.js
@@ -1,5 +1,16 @@
 const Link = artifacts.require("Link");
+const Wallet = artifacts.require("Wallet");
 
-module.exports = function (deployer) {
-  deployer.deploy(Link);
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(Link);
+  let wallet = await Wallet.deployed();
+  let link = await Link.deployed();
+
+  await link.approve(wallet.address, 500);
+  await wallet.addToken(web3.utils.fromUtf8("LINK"), link.address);
+  await wallet.deposit(100, web3.utils.fromUtf8("LINK"));
+
+  let balanceOfLink = await wallet.balances(accounts[0], web3.utils.fromUtf8("LINK"));
+  console.log(balanceOfLink);
+
 };
