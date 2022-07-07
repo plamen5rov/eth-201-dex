3e16f97ba9ca0acf9ee612ad4d0818fb3dcc91b7
Plamen <biznetmen@gmail.com>
2022-07-07 08:26:32 +0300

    dex contract initial setup

---
+++ contracts/dex.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./wallet.sol";
+
+contract Dex is Wallet {
+    enum Side {
+        BUY,
+        SELL
+    }
+
+    struct Order {
+        uint256 id;
+        address trader;
+        Side side;
+        bytes32 ticker;
+        uint256 amount;
+        uint256 price;
+    }
+
+    mapping(bytes32 => mapping(uint256 => Order[])) orderBook;
+
+    function getOrderBook(bytes32 ticker, Side side)
+        public
+        view
+        returns (Order[] memory)
+    {
+        return orderBook[ticker][uint256(side)];
+    }
+}
